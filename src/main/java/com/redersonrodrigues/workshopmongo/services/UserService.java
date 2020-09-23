bf17e3f8f42840e8fe3134378a7714e49d547aca
Rederson Rodrigues <redersonatalibarodrigues@gmail.com>
2020-09-23 17:20:06 -0300

    Obtendo um usuário por id

--- src/main/java/com/redersonrodrigues/workshopmongo/services/UserService.java
+++ src/main/java/com/redersonrodrigues/workshopmongo/services/UserService.java
@@ -1,22 +1,27 @@
 package com.redersonrodrigues.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.redersonrodrigues.workshopmongo.domain.User;
 import com.redersonrodrigues.workshopmongo.repository.UserRepository;
+import com.redersonrodrigues.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
 
-	
-	public List<User> findAll(){
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
