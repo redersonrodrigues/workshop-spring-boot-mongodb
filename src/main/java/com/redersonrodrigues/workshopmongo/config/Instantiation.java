b576a53ab78ad03ab8d3ce831604b0cf813db2e8
Rederson Rodrigues <redersonatalibarodrigues@gmail.com>
2020-09-24 09:08:28 -0300

    Referenciando os posts do usuário

--- src/main/java/com/redersonrodrigues/workshopmongo/config/Instantiation.java
+++ src/main/java/com/redersonrodrigues/workshopmongo/config/Instantiation.java
@@ -25,24 +25,26 @@ public class Instantiation implements CommandLineRunner {
 
 	@Override
 	public void run(String... arg0) throws Exception {
-
+		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-
+		
 		userReposiroty.deleteAll();
 		postReposiroty.deleteAll();
-
+		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
+		
 		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
 
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
-				new AuthorDTO(maria));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
 		postReposiroty.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userReposiroty.save(maria);
 	}
 
 }
